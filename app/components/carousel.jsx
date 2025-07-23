20ed9433624df8f3b22493639daeab252c35e241
DESKTOP-QINRQIA\nicol <nicolepanggg@gmail.com>
2025-07-24 01:01:57 +0800

    carouel

--- app/components/carousel.jsx
+++ app/components/carousel.jsx
@@ -1,18 +1,14 @@
 import { useState } from "react";
 
-
 export default function Carousel({ children: slides }) {
     const [curr, setCurr] = useState(0);
-
     const prev = () => setCurr((curr)=>(curr==0? slides.length-1:curr-1));
     const next = () => setCurr((curr)=>(curr==slides.length-1? 0:curr+1));
 
-
-
     return (
         <div className="carousel">
-            <div className="flex transition-transform ease-out duration-500" style={{transform:`translateX(-${curr*100}%)`}}>
-                {slides.map((i) => (<img src={i} />))}
+            <div className="scroll-bar" style={{transform:`translateX(-${curr*100}%)`}}>
+                {slides.map((i) => (<img src={i}/>))}
             </div>
             <div className="arrow">
                 <button onClick={prev}>
